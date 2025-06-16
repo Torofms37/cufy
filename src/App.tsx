61bf19dd305d3fffc6c9d38ea68d59131c55c959
torofms37 <torofms37@gmail.com>
2025-06-16 10:51:36 -0700

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function App() {
   return (
     <div>
       <Navbar />
+      <div className='container-home'>
+        <i className="iconoir-home"></i>
+        <h1>Café para todos</h1>
+        <h3>Un café según el gusto de la persona</h3>
+        <button>MÁS INFORMACIÓN</button>
+      </div>
     </div>
   )
 }
