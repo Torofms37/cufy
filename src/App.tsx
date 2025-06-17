902467680798354e4e6d73870dfabdccaed59a96
torofms37 <torofms37@gmail.com>
2025-06-16 23:09:08 -0700

    v.1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+
 import "./index.css";
+import { Icons } from "./ui/Icons";
 import { Navbar } from "./ui/Navbar";
+
 
 function App() {
   return (
@@ -7,11 +10,12 @@ function App() {
       <Navbar />
       <div className="first-container">
         <div className="container-home">
-          <i className="iconoir-home"></i>
+          <i className="iconoir-home icon-a"></i>
           <h1>Café para todos</h1>
           <h3>Un café según el gusto de la persona</h3>
           <button>MÁS INFORMACIÓN</button>
         </div>
+      <Icons />
       </div>
     </div>
   );
---
+++ src/ui/Icons.tsx
@@ -0,0 +1,10 @@
+export const Icons = () => {
+  return (
+    <div className="icons-footer icon-f">
+      <i className="iconoir-facebook icon-f"></i>
+      <i className="iconoir-x icon-f"></i>
+      <i className="iconoir-whatsapp icon-f"></i>
+      <i className="iconoir-instagram icon-f"></i>
+    </div>
+  );
+};
