2fd7e19c7665819e83b1ce8a32c9c38f921e3173
torofms37 <torofms37@gmail.com>
2025-06-16 20:13:11 -0700

    v.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import './index.css'
-import { Navbar } from './ui/Navbar'
+import "./index.css";
+import { Navbar } from "./ui/Navbar";
 
 function App() {
-
   return (
     <div>
       <Navbar />
-      <div className='container-home'>
-        <i className="iconoir-home"></i>
-        <h1>Café para todos</h1>
-        <h3>Un café según el gusto de la persona</h3>
-        <button>MÁS INFORMACIÓN</button>
+      <div className="first-container">
+        <div className="container-home">
+          <i className="iconoir-home"></i>
+          <h1>Café para todos</h1>
+          <h3>Un café según el gusto de la persona</h3>
+          <button>MÁS INFORMACIÓN</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
