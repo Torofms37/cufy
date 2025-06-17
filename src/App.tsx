1a198e0d2c5ddba32b8c797edee073d014ae6cc3
torofms37 <torofms37@gmail.com>
2025-06-17 00:27:14 -0700

    v.1.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-
 import "./index.css";
 import { Icons } from "./ui/Icons";
 import { Navbar } from "./ui/Navbar";
-
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="first-container">
+
+      <section className="first-container">
         <div className="container-home">
           <i className="iconoir-home icon-a"></i>
           <h1>Café para todos</h1>
           <h3>Un café según el gusto de la persona</h3>
           <button>MÁS INFORMACIÓN</button>
         </div>
-      <Icons />
-      </div>
+        <Icons />
+      </section>
+
+      <section className="second-container">
+        <h1>Café para todos</h1>
+        <div className="container-second">
+          <div className="container-second-right">
+            <p>
+              Café de cualquier marca para empresas, gobierno y eventos en
+              México. Calidad, sabor y entrega eficiente para cada reunión y
+              jornada laboral.
+            </p>
+            <span>¡El café perfecto, cuando y donde lo necesites!</span>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
-
 export default App;
