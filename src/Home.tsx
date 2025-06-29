b79a2dd97a4966b0a91e4d0e21ebd74dca5b27ec
torofms37 <torofms37@gmail.com>
2025-06-29 09:47:55 -0700

    v1.8

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,39 @@
 import "./index.css";
-import { AppRouter } from "./router/AppRouter";
+import { Footer } from "./ui/Footer";
+import { Icons } from "./ui/Icons";
+import { Navbar } from "./ui/Navbar";
+
 
 function Home() {
-  return (
-    <AppRouter />
+return (
+    <div>
+      <Navbar />
+
+      <section className="first-container">
+        <div className="container-home">
+          <i className="iconoir-home icon-a"></i>
+          <h1>Café para todos</h1>
+          <h3>Un café según el gusto de la persona</h3>
+          <button>MÁS INFORMACIÓN</button>
+        </div>
+        <Icons />
+      </section>
+
+      <section className="second-container">
+        <h1>Café para todos</h1>
+        <div className="container-second">
+          <div className="container-second-right">
+            <p>
+              Café de cualquier marca para empresas, gobierno y eventos en
+              México. Calidad, sabor y entrega eficiente para cada reunión y
+              jornada laboral.
+            </p>
+            <span>¡El café perfecto, cuando y donde lo necesites!</span>Add commentMore actions
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
   );
 }
 export default Home;
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,6 +1,43 @@
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className="register-page">
+
+      <div className="ya-login">
+        <h3>¿Ya tienes cuenta?</h3>
+        <button className="btn-submit">INICIA SESIÓN</button>
+      </div>
+
+      <div className="fields-form">
+        <h3>Llena los campos</h3>
+
+        <form>
+          <div className="field">
+            <label htmlFor="name">Nombre</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="field">
+            <label htmlFor="email">Correo electrónico</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <div className="field">
+            <label htmlFor="confirm-password">Confirmar contraseña</label>
+            <input type="password" id="confirm-password" name="confirm-password" required />
+          </div>
+          <div className="field">
+            <label htmlFor="terms">
+              <input type="checkbox" id="terms" name="terms" required />
+              Acepto los términos y condiciones
+            </label>
+            <button className="btn-submit">Registrarse</button>
+          </div>
+        </form>
+
+      </div>
+    </div>
   )
 }
