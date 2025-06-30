fabe61d3f0e6e09e707b2e8b864dbf6b0b25d709
torofms37 <torofms37@gmail.com>
2025-06-29 23:29:03 -0700

    [

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,6 +1,36 @@
+import "../../styles/loginpage.css";
+import Cafe from '../../assets/bringcafe.png'
+
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="register-page">
+      <h2>Inicia sesión</h2>
+
+      <div className="fields-form">
+        <h3>Llena los campos</h3>
+
+        <form className="form-fields">
+          <div className="field">
+            <label htmlFor="email">Correo electrónico</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+        </form>
+        <button type="submit" className="btn-submit">
+          Iniciar sesión
+        </button>
+
+        <img className="img-cafe" src={Cafe} alt="bring cafe" />
+      </div>
+
+      <div className="ya-login">
+        <h3>¿Todavía no tienes cuenta?</h3>
+        <button className="btn-submit">Registrate</button>
+      </div>
+    </div>
+  );
+};
