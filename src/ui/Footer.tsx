ccf9c1c189dabde88bdaf05372a940afddf19adf
Eduardo_S <segura2011@live.com>
2025-07-02 03:18:01 -0600

    [STYLE]: Estilos a los TyC, añade apartado en Footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
 import '../styles/footer.css'
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; 2025 <strong>Cufy</strong> . Todos los derechos reservados.</p>
+      <Link to="/terminosCondiciones">
+        <p className="tyc">Terminos y condiciones</p>
+      </Link>
       <div className="footer-links">
 
       </div>
